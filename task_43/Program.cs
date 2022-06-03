bb2c867e14542bc0c95b582a912289d6a24c020f
Lena <eseliver@icloud.com>
2022-06-03 21:14:29 +0300

    43

--- task_43/Program.cs
+++ task_43/Program.cs
@@ -2,13 +2,20 @@
 // y = k1 *x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // example: b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+double[] FindCoord(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    double[] coord = new double[2];
+    coord[0] = x;
+    coord[1] = y;
+    return coord;
+}
 Console.Write("Введите значения b1, k1, b2, k2 через пробел: ");
-double[] array = Console.ReadLine().Split().Select(double.Parse).ToArray();
-Console.WriteLine(String.Join(" ", array));
-double b1 = array[0];
-double k1 = array[1];
-double b2 = array[2];
-double k2 = array[3];
-double x = (b2 - b1) / (k1 - k2);
-Console.Write($"Координаты точки пересечения ({x} , {k1 * x + b1})");
-
+string[] array = Console.ReadLine().Split(" ");
+double b1 = double.Parse(array[0]);
+double k1 = double.Parse(array[1]);
+double b2 = double.Parse(array[2]);
+double k2 = double.Parse(array[3]);
+double[] result = FindCoord(b1, k1, b2, k2);
+Console.Write($"Координаты точки ({result[0]}, {result[1]})");
